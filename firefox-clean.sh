37be56e3648015c47152ff91bfa98fa14f6f56b3
arnaudbey <arnaud.bey@gmail.com>
2016-10-05 18:04:29 +0200

    display number of deleted rows

--- firefox-clean.sh
+++ firefox-clean.sh
@@ -5,6 +5,7 @@ do
 	KEYWORD=$1
 	for f in $FILES
 	do
+		echo $(sqlite3 $f "select count(*) FROM moz_places WHERE moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%';") suppression pour $KEYWORD
 	    sqlite3 $f "DELETE FROM moz_places WHERE moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%';"
 	done
 	shift
