0f92f9fb9a19ee52274e2a79c4642658c42e7068
arnaudbey <arnaud.bey@gmail.com>
2016-10-07 12:45:46 +0200

    fix bookmarked place being deleted

--- firefox-clean.sh
+++ firefox-clean.sh
@@ -3,10 +3,11 @@ FILES=~/.mozilla/firefox/*/places.sqlite
 while [ $# -gt 0 ]
 do
 	KEYWORD=$1
+	CRITERIA="FROM moz_places WHERE (moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%') AND NOT EXISTS (SELECT * FROM moz_bookmarks WHERE moz_bookmarks.fk = moz_places.id)"
 	for f in $FILES
 	do
-		echo $(sqlite3 $f "select count(*) FROM moz_places WHERE moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%';") suppression pour $KEYWORD
-	    sqlite3 $f "DELETE FROM moz_places WHERE moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%';"
+		echo $(sqlite3 $f "SELECT COUNT(id) $CRITERIA") "> $KEYWORD "
+	    sqlite3 $f "DELETE $CRITERIA"
 	done
 	shift
 done
