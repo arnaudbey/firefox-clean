1a45a7ea64d71f1d505c40c81586ccc85c36da04
arnaudbey <arnaud.bey@gmail.com>
2017-01-20 18:52:30 +0100

    add sqlite opti

--- firefox-clean.sh
+++ firefox-clean.sh
@@ -1,13 +1,21 @@
-FILES=~/.mozilla/firefox/*/places.sqlite
-
+HISTORYFILES=~/.mozilla/firefox/*/places.sqlite
 while [ $# -gt 0 ]
 do
 	KEYWORD=$1
 	CRITERIA="FROM moz_places WHERE (moz_places.url LIKE '%$KEYWORD%' OR moz_places.title LIKE '%$KEYWORD%') AND NOT EXISTS (SELECT * FROM moz_bookmarks WHERE moz_bookmarks.fk = moz_places.id)"
-	for f in $FILES
+	for f in $HISTORYFILES
 	do
 		echo $(sqlite3 $f "SELECT COUNT(id) $CRITERIA") "> $KEYWORD "
 	    sqlite3 $f "DELETE $CRITERIA"
 	done
 	shift
 done
+
+echo "OPTI SQLITE"
+ALLFILES=~/.mozilla/firefox/*/*.sqlite
+for f in $ALLFILES
+do
+	echo "..."
+	sqlite3 $f 'VACUUM;'
+done
+echo "done."
